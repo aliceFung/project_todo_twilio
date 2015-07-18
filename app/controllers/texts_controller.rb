01f0d853cec4c518226f1cdd91e99f24022ecd90
alokp <alokpra@gmail.com>
2015-07-18 16:59:27 -0700

    twilio working

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,26 +1,21 @@
 class TextsController < ApplicationController
 
-
-
-
-  def send_text
-
-    # acct_sid = ENV["twilio_sid"]
-    # auth_token = ENV["twilio_auth_token"]
-    @client = Twilio::REST::Client.new ENV["twilio_sid"], ENV["twilio_auth_token"]
-    @client.messages.create( from: '', #set num
-      to: '', #set number
-      body: "string" #through params
-      )
-    # msg.to
+  	def send_text
+	   	a = Todo.find(params[:id])
+	    acct_sid = ENV["twilio_sid"]
+	    auth_token = ENV["twilio_auth_token"]
+	    from_ph = ENV["twilio_num"]
+	    to_ph = ENV["my_phone"]
+	    @client = Twilio::REST::Client.new acct_sid, auth_token
+	    if @client.messages.create(from: from_ph,
+	      to: to_ph,
+	      body: a.text
+	      )
+	    	flash[:success] = "Text sent"
+	    else
+	    	flash[:error] = "Could not send text"
+	    end
+	    redirect_to todos_path
 	end
-
-  private
-
-  def params_body
-
-  end
-
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'todos#index'
-
   resources :todos, only: [:index, :create, :destroy, :edit, :update]
-
-  post "send" => "texts#send_text"
+  post "send_text/:id", to: "texts#send_text", as: "send_text"
 
 end
