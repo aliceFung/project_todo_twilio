5cb3eeac56de7fede78091df3b969517e7de780c
Alice Fung <alice.o.fung@gmail.com>
2015-07-18 19:23:09 -0400

    twilio...

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,7 +1,26 @@
 class TextsController < ApplicationController
 
-	def send_text
 
+
+
+  def send_text
+
+    # acct_sid = ENV["twilio_sid"]
+    # auth_token = ENV["twilio_auth_token"]
+    @client = Twilio::REST::Client.new ENV["twilio_sid"], ENV["twilio_auth_token"]
+    @client.messages.create( from: '', #set num
+      to: '', #set number
+      body: "string" #through params
+      )
+    # msg.to
 	end
+
+  private
+
+  def params_body
+
+  end
+
+
 
 end
